0ed788f3a9ac36a2e37983e175c2f020cb56fa3a
arnabh-ban07 <banerjee.arnabh@gmail.com>
2024-06-22 21:49:03 +0530

    main

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,34 +1,40 @@
 
+// src/components/Pagination.js
 import React from 'react';
 import { Pagination as BootstrapPagination } from 'react-bootstrap';
 
 const Pagination = ({ currentPage, totalResults, pageSize, onPageChange }) => {
   const totalPages = Math.ceil(totalResults / pageSize);
 
-  const handlePageChange = (page) => {
-    if (page >= 1 && page <= totalPages) {
-      onPageChange(page);
-    }
+  const handlePageChange = (pageNumber) => {
+    onPageChange(pageNumber);
   };
 
   return (
-    <BootstrapPagination>
+    <BootstrapPagination className="justify-content-center">
       <BootstrapPagination.First onClick={() => handlePageChange(1)} />
-      <BootstrapPagination.Prev onClick={() => handlePageChange(currentPage - 1)} />
-      {[...Array(totalPages)].map((_, index) => (
+      <BootstrapPagination.Prev
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      />
+      {Array.from({ length: totalPages }, (_, index) => (
         <BootstrapPagination.Item
-          key={index + 1}
+          key={index}
           active={index + 1 === currentPage}
           onClick={() => handlePageChange(index + 1)}
         >
           {index + 1}
         </BootstrapPagination.Item>
       ))}
-      <BootstrapPagination.Next onClick={() => handlePageChange(currentPage + 1)} />
+      <BootstrapPagination.Next
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      />
       <BootstrapPagination.Last onClick={() => handlePageChange(totalPages)} />
     </BootstrapPagination>
   );
 };
 
 export default Pagination;
+
 
